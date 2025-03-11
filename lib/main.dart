1b45e3ffffe4802a665d891b723970865f5534fe
Mirza <2022453238@student.uitm.edu.my>
2025-03-12 03:35:21 +0800

    edit card on  login page

--- lib/main.dart
+++ lib/main.dart
@@ -50,104 +50,137 @@ class LoginPage extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.black,
           image: DecorationImage(
-            image: AssetImage('assets/background.png'), // Add your background image here
+            image: AssetImage(
+                'assets/background.png'), // Add your background image here
             fit: BoxFit.cover,
           ),
         ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Card(
-              color: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              elevation: 5,
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      'Login',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'aileron',
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    TextField(
-                      controller: _emailController, // Use the controller here
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    TextField(
-                      controller:
-                          _passwordController, // Use the controller here
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        _login(context);
-                      },
-                      child: Text('Log In',
-                        style: TextStyle(
-                        fontSize: 11,
-                        fontFamily: 'Poppins Bold',
-                        color: Colors.white,
-                      ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFF6e9277), // Button color
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    TextButton(
-                      onPressed: () {
-                        // Handle forgot password action
-                      },
-                      child: Text('FORGOT PASSWORD?',
-                        style: TextStyle(
-                          fontSize: 9.3,
-                          fontFamily: 'DM Sans',
-                          color: Colors.grey,
-                         ),
+        child: Column(
+          children: [
+            // Upper half for background image
+            Expanded(
+              flex: 1, // This takes half of the screen
+              child: Container(),
+            ),
+            // Lower half for the card
+            Expanded(
+              flex: 0, // This takes the other half of the screen
+              child: Container(
+                margin: EdgeInsets.zero, // Remove any margin
+                child: Card(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(30)), // Rounded top corners
+                  ),
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'aileron',
+                          ),
                         ),
-                    ),
-                    SizedBox(height: 10),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  SignUpPage()), // Navigate to SignUpPage
-                        );
-                      },
-                      child: Text('SIGN UP HERE',
-                        style: TextStyle(
-                          fontSize: 9.3,
-                          fontFamily: 'DM Sans',
-                          color: Colors.grey,
+                        SizedBox(height: 20),
+                        TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            labelStyle: TextStyle(
+                              fontFamily: 'DM Sans',
+                            ),
+                            filled: true, // Enable fill color
+                            fillColor: Color(0xFFE0E0E0), // Set fill color
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.circular(20), // Circular border
+                              borderSide: BorderSide.none, // Remove border line
+                            ),
+                          ),
                         ),
-                      ),
+                        SizedBox(height: 10),
+                        TextField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(
+                              fontFamily: 'DM Sans',
+                            ),
+                            filled: true, // Enable fill color
+                            fillColor: Color(0xFFE0E0E0), // Set fill color
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.circular(20), // Circular border
+                              borderSide: BorderSide.none, // Remove border line
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            _login(context);
+                          },
+                          child: Text(
+                            'Log In',
+                            style: TextStyle(
+                              fontSize: 11,
+                              fontFamily: 'DM Sans',
+                              color: Colors.white,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF6e9277), // Button color
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 50, vertical: 15),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      SignUpPage()), // Navigate to SignUpPage
+                            );
+                          },
+                          child: Text(
+                            'SIGN UP HERE!',
+                            style: TextStyle(
+                              fontSize: 10,
+                              fontFamily: 'DM Sans',
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        TextButton(
+                          onPressed: () {
+                            // Handle forgot password action
+                          },
+                          child: Text(
+                            'FORGOT PASSWORD?',
+                            style: TextStyle(
+                              fontSize: 9.3,
+                              fontFamily: 'DM Sans',
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
