f0043abf558f4db848369d4abb88a7ff218346d2
Mirza <2022453238@student.uitm.edu.my>
2025-03-24 01:40:41 +0800

    sign up page added

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -47,85 +47,143 @@ class _SignUpPageState extends State<SignUpPage> {
         decoration: BoxDecoration(
           color: Colors.black,
           image: DecorationImage(
-            image: AssetImage(
-                'assets/PNG/background.png'), // Add your background image here
+            image: AssetImage('assets/PNG/background.png'),
             fit: BoxFit.cover,
           ),
         ),
-        padding: EdgeInsets.all(20.0),
-        child: Card(
-          elevation: 5,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Create an Account',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 20),
-                // Name Field
-                TextField(
-                  controller: _nameController,
-                  decoration: InputDecoration(
-                    labelText: 'Name',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                // Username Field
-                TextField(
-                  controller: _usernameController,
-                  decoration: InputDecoration(
-                    labelText: 'Username',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                // Email Field
-                TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                // Password Field
-                TextField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () => _signUp(context),
-                  child: Text('Sign Up'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context); // Go back to the login page
-                  },
-                  child: Text('Already have an account? Log in'),
-                ),
-              ],
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(),
             ),
-          ),
+            Expanded(
+              flex: 0,
+              child: Container(
+                margin: EdgeInsets.zero,
+                height: 550,
+                width: 500,
+                child: Card(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(30),
+                    ),
+                  ),
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.all(30),
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'aileron'),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        // Email Field
+                        TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            labelStyle: TextStyle(
+                              fontFamily: 'DM Sans',
+                            ),
+                            filled: true, // Enable fill color
+                            fillColor: Color(0xFFE0E0E0), // Set fill color
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.circular(20), // Circular border
+                              borderSide: BorderSide.none, // Remove border line
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        // Password Field
+                        TextField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(
+                              fontFamily: 'DM Sans',
+                            ),
+                            filled: true, // Enable fill color
+                            fillColor: Color(0xFFE0E0E0), // Set fill color
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.circular(20), // Circular border
+                              borderSide: BorderSide.none, // Remove border line
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        // Name Field
+                        TextField(
+                          controller: _nameController,
+                          decoration: InputDecoration(
+                            labelText: 'Name',
+                            labelStyle: TextStyle(
+                              fontFamily: 'DM Sans',
+                            ),
+                            filled: true, // Enable fill color
+                            fillColor: Color(0xFFE0E0E0), // Set fill color
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.circular(20), // Circular border
+                              borderSide: BorderSide.none, // Remove border line
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        // Username Field
+                        TextField(
+                          controller: _usernameController,
+                          decoration: InputDecoration(
+                            labelText: 'Username',
+                            labelStyle: TextStyle(
+                              fontFamily: 'DM Sans',
+                            ),
+                            filled: true, // Enable fill color
+                            fillColor: Color(0xFFE0E0E0), // Set fill color
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.circular(20), // Circular border
+                              borderSide: BorderSide.none, // Remove border line
+                            ),
+                          ),
+                        ),
+                        // Sign Up Button
+                        Container(
+                          margin: EdgeInsets.all(20),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              _signUp(context);
+                            },
+                            child: Text('Sign Up', style: TextStyle(fontSize: 11, fontFamily: 'DM Sans', color: Colors.white),),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xFF6e9277), // Button color
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 40,
+                                vertical: 10,
+                              ),  
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
